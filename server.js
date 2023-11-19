1769503b39ac8a771f41a1bb4f73e6e389852eb9
Mallikarjun362 <pmallikarjun307@gmail.com>
2023-11-19 13:45:03 +0530

    added express

--- server.js
+++ server.js
@@ -1,31 +1,44 @@
-// SECTION: CONSTANTS 
-const SOCKET_SERVER_PORT = 3001
-
-// SECTION: SERVER INITIALIZATION
-const server = require("socket.io")(SOCKET_SERVER_PORT, {
+const SOCKET_SERVER_PORT = 8000
+const express = require("express")
+const app = express()
+const http = require("http").Server(app)
+const io = require("socket.io")(http, {
     cors: ["*"]
 })
 
+
 console.log("\n----------- Server Started -----------------\n");
 
-// SECTION: NAMESPACE IMPLEMENTATION
-server.on('connection', (socket) => {
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+io.on('connection', (socket) => {
     console.log("New Connection:", socket.id);
     socket.on('send-data-to-room', (room, data) => {
         console.log(data);
         socket.to(room).emit('receive-data', data)
     })
-    // ------ ROOM MANAGEMENT ------
+
     socket.on('join-room', (room) => {
         console.log("Join Room", socket.id, "->", room);
         socket.join(room)
     })
+
     socket.on('exit-room', (room) => {
         console.log("Exit Room", socket.id, room);
         socket.leave(room)
     })
+
     socket.on('disconnect', () => {
         console.log("Disconnect :", socket.id);
     })
+
+    socket.on('ping', (msg) => {
+        socket.emin('ping', msg)
+    })
 })
 
+http.listen(SOCKET_SERVER_PORT, () => {
+    console.log(`App listening on port ${SOCKET_SERVER_PORT}`)
+});
